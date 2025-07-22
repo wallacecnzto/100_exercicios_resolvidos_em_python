76cfd8b5cd3da1b4aeff96a5e3aeded19585da92
Wallace Silva <wallacewvinfo@gmail.com>
2025-07-22 19:31:13 -0300

    Finalizando exercícios de fórmulas matemáticas

--- formulas_matematicas/ex15.py
+++ formulas_matematicas/ex15.py
@@ -2,6 +2,7 @@
 15. Crie um programa que solicite ao usuário o valor do raio de uma esfera e calcule e exiba o seu volume.
 Fórmula: V = (4/3) * pi * raio ao cubo.
 """
+
 import math
 
 raio = float(input("Digite o raio da esfera: "))
