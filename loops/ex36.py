76cfceccc5f9a3accaee99c74613f945fab6aff3
Wallace Mariano <wallacewvinfo@gmail.com>
2025-08-07 20:14:56 -0300

    Primeiro commit da lista de exercícios de listas de repetição.

---
+++ loops/ex36.py
@@ -0,0 +1,11 @@
+"""
+36. Escreva um programa que exiba os números de 1 a 10 utilizando um laço de repetição
+"""
+number = 1
+
+# while number < 11:
+#     print(number)
+#     number += 1
+
+for num in range(1,11):
+    print(num)
