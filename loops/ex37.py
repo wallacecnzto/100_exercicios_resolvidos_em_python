b20d1326f82a5794420c3beca1fa96064fa11efe
Wallace Mariano <wallacewvinfo@gmail.com>
2025-08-10 17:58:24 -0300

    Fazendo exercicios 37 e 38.

---
+++ loops/ex37.py
@@ -0,0 +1,6 @@
+"""
+37. Escreva um programa que exiba todos os números de 1 a 100
+"""
+
+for n in range(1, 101):
+    print(n)
---
+++ loops/ex38.py
@@ -0,0 +1,11 @@
+"""
+38. Escreva um programa que exiba todos os números pares de 1 a 100.
+usando o while
+"""
+
+n = 1
+
+while n <= 100:
+    if n % 2 == 0:
+        print(n)
+    n += 1
