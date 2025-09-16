0adc29e873a55b1c7cd6561a772c6843517aae38
Wallace Mariano <wallacewvinfo@gmail.com>
2025-09-15 22:02:59 -0300

    :leapstick: feat: Finalizando exercício 36

---
+++ condicionais/ex36.py
@@ -0,0 +1,14 @@
+# Escreva um programa que exiba os números de 1 a 10 utilizando um laço de repetição
+
+# usando while
+
+number = 1
+
+while number <= 10:
+    print(number)
+    number += 1
+
+# usando for
+
+for i in range(1, 11):
+    print(i)
