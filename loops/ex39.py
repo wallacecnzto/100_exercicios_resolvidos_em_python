6819ed962284a645d4bb832613db933489a676b1
Wallace Mariano <wallacewvinfo@gmail.com>
2025-10-13 19:19:51 -0300

    Update arquivo do exercicio 39

--- loops/ex39.py
+++ loops/ex39.py
@@ -1,6 +1,7 @@
-'''
-39. Escreva um programa que exiba os números pares de 1 a 50 e os números ímpares de 51 a 100 utilizando um laço de repetição.
-'''
+"""
+39. Escreva um programa que exiba os números pares de 1 a 50 e os números
+ímpares de 51 a 100 utilizando um laço de repetição.
+"""
 
 for i in range(1, 51):
     if i % 2 == 0:
